cfa916763063ac1135084243e73611070a6ff619
zara-sh <zara.regist@gmail.com>
2017-10-10 11:42:40 +0900

    Add status to Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010023558) do
+ActiveRecord::Schema.define(version: 20171010024014) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
 end
